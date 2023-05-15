80945010238b37e5f9accbd8c9870e9aaa2bc72d
tirth272 <shahtirth24680@gmail.com>
2023-05-15 09:23:57 +0530

    h

---
+++ Module-1/1.py
@@ -0,0 +1,9 @@
+num=int(input("Enter A number: "))
+print(num)
+
+if(num>0):
+    print("The number you entered is positive")
+elif(num<0):
+    print("The number you entered is negative")
+else:
+    print("The number you entered is zero")
